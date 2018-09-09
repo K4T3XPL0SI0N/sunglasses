dc55c746e0107e38f1c2a6b80cdd479eec25f6ca
boopdev <40839690+boopdev@users.noreply.github.com>
2018-09-09 03:03:35 -0300

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -8,12 +8,19 @@ class Fun:
         self.client = client
     
     @commands.command()
-    async def penis(self, ctx, member : discord.Member):
-      if member.id not in current_dicks:
-        current_dicks[member.id] = "8" + "="*random.randint(1,20) +"D"
-        await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id]))
-      else:
-        await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id]))
+    async def penis(self, ctx, member : discord.Member = None):
+        if member not in (None, ()):
+          if member.id not in current_dicks:
+            current_dicks[member.id] = "8" + "="*random.randint(1,20) +"D"
+            await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id]))
+          else:
+            await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id]))
+        else:
+            if ctx.author.id not in current_dicks:
+            current_dicks[ctx.author.id] = "8" + "="*random.randint(1,20) +"D"
+            await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[ctx.author.id]))
+          else:
+            await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[ctx.author.id]))
                  
 def setup(client):
     client.add_cog(Fun(client))
