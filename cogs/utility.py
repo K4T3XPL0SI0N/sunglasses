375fa089029187496ebffa85b58dc10b03f44aee
boopdev <40839690+boopdev@users.noreply.github.com>
2018-09-09 02:00:51 -0300

    Update utility.py

--- cogs/utility.py
+++ cogs/utility.py
@@ -1,1 +1,34 @@
+import discord, asyncio, os
+from discord.ext import commands
 
+class Utility:
+    def __init__(self, client):
+        self.client = client
+
+    @commands.command()
+    @commands.is_owner()
+    async def eval(self, ctx, *, code):
+        """Evaluates Code"""
+        code = code.strip("` ")
+        pyform = '```python\n{}\n```'
+
+
+        embedD = discord.Embed(colour=0xDEADBF)
+
+        try:
+            result = eval(code)
+            if inspect.isawaitable(result):
+                result = await result
+            print(result)
+        except Exception as e:
+            embedD.add_field(name="Input",value=pyform.format(code), inline=False)
+            embedD.add_field(name="Output", value=pyform.format(type(e).__name__, e), inline=False)
+            await ctx.send(embed=embedD)
+        else:
+            embedD.add_field(name="Input",value=pyform.format(code), inline=False)
+            embedD.add_field(name="Output", value=pyform.format(result), inline=False)
+            await ctx.send(embed=embedD)
+        await ctx.message.delete()
+        
+def setup(client):
+    client.add_cog(Utility(client))
