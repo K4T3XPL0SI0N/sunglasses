8ce82a17a92f589f390ecdd228293f6d534376f7
boopdev <40839690+boopdev@users.noreply.github.com>
2018-09-09 02:48:48 -0300

    Create fun.py

---
+++ cogs/fun.py
@@ -0,0 +1,19 @@
+import discord, asyncio, random
+from discord.ext import commands
+
+current_dicks = {}
+
+class Fun:
+    def __init__(self, client):
+    self.client = client
+    
+    @commands.command()
+    async def penis(self, member : discord.Member):
+      if member.id not in current_dicks:
+        current_dicks[member.id] = "8" + "="*random.randint(1,20) "D"
+        await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id]))
+      else:
+        await ctx.send(await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id]))
+        
+def setup(client):
+  client.add_cog(Fun(client))
