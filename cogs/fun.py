3751d8bef74ac3dfad78f61f70532285c0e723f8
boopdev <40839690+boopdev@users.noreply.github.com>
2018-09-09 03:01:00 -0300

    Update fun.py

--- cogs/fun.py
+++ cogs/fun.py
@@ -13,7 +13,7 @@ class Fun:
         current_dicks[member.id] = "8" + "="*random.randint(1,20) +"D"
         await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id]))
       else:
-        await ctx.send(await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id])))
+        await ctx.send("{0.mention}'s dick: **{1}**".format(member, current_dicks[member.id]))
                  
 def setup(client):
     client.add_cog(Fun(client))
