85f7e622b7e069aa6fbba50c9a0912c2b527ff22
boopdev <40839690+boopdev@users.noreply.github.com>
2018-09-09 21:24:49 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 sys.path.insert(0, './cogs')
 
 bot = commands.Bot(command_prefix='j')
-all_cogs = ['utility', 'fun'] # list of cogs
+all_cogs = ['utility', 'fun', 'info'] # list of cogs
 
 @bot.event
 async def on_ready():
