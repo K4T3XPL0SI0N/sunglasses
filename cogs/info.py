87b343255367c9f910c12d48b7e7c6959f9c674c
boopdev <40839690+boopdev@users.noreply.github.com>
2018-09-09 21:29:03 -0300

    Update info.py

--- cogs/info.py
+++ cogs/info.py
@@ -10,7 +10,7 @@ class Information:
         """Get information about the bot"""
         _appInfo = await self.client.application_info()
         await ctx.send(embed=discord.Embed(title="{0.name}".format(_appInfo),
-                                           description = "{0.name} is owned by *`{0.owner.name}`* with :heart:\n{0.name} was originally made by <@104660856658210816>".format(_appInfo),
+                                           description = "{0.name} is owned by *`{0.owner}`* with :heart:\n{0.name} was originally made by <@104660856658210816>".format(_appInfo),
                                            colour=0x9E1A1A))
                        
     @commands.command()
