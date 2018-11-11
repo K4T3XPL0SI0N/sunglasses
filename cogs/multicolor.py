98c107eab89112e5ad37198791698d8a6f613f7f
boopdev <40839690+boopdev@users.noreply.github.com>
2018-11-11 15:12:27 -0400

    Update multicolor.py

--- cogs/multicolor.py
+++ cogs/multicolor.py
@@ -28,8 +28,8 @@ class ColorShid():
         role = get_role_id(guild.roles, INFORMATION['ROLE ID'])       
 
         while True:
-            role.edit(colour=discord.Color.from_rgb(r=random.randint(0,255), g=random.randint(0,255), b=random.randint(0,255)), reason="Automatic Color Switch")
-            asyncio.sleep(INFORMATION['SHIFT DELAY'])
+            await role.edit(colour=discord.Color.from_rgb(r=random.randint(0,255), g=random.randint(0,255), b=random.randint(0,255)), reason="Automatic Color Switch")
+            await asyncio.sleep(INFORMATION['SHIFT DELAY'])
 
 def setup(client):
     client.add_cog(ColorShid(client))
