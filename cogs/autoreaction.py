40df6e2370b96b4b46b36ecde2cfdd9360ec935f
boopdev <40839690+boopdev@users.noreply.github.com>
2018-11-14 18:59:35 -0400

    Update autoreaction.py

--- cogs/autoreaction.py
+++ cogs/autoreaction.py
@@ -1,1 +1,21 @@
+import discord, asyncio
+from discord.ext import commands
 
+triggers = {
+    "jake" : "<:jake:505181297963040768>"
+}
+
+class AutoReactor():
+    def __init__(self, client):
+        self.client = client
+        
+    async def on_message(self, msg):
+        for trigger in triggers:
+            if trigger in msg.content.lower():
+                try:
+                    await msg.add_reaction(trigger[trigger])
+                except discord.Forbidden:
+                    pass
+                
+def setup(client):
+    client.add_cog(AutoReactor(client))
