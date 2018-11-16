a9fa5abdfbadbd40317dc5775b15835bc6e4d7d7
boopdev <40839690+boopdev@users.noreply.github.com>
2018-11-16 00:08:30 -0400

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 
 def get_role_id(role_id, guild):
     for r in guild.roles:
-        if r.id = role_id:
+        if r.id == role_id:
             return r
 
 class Moderation:
