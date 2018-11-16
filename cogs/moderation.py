a0509b9ded344b78aa0fd41a1ed35c3fe01cc3be
boopdev <40839690+boopdev@users.noreply.github.com>
2018-11-15 23:56:56 -0400

    Update moderation.py

--- cogs/moderation.py
+++ cogs/moderation.py
@@ -8,7 +8,7 @@ class Moderation:
     async def on_message(self, msg):
         guild = self.client.get_guild(468119888058122241)
         
-        if msg.channel = guild.get_channel(487768754155945995):
+        if msg.channel == guild.get_channel(487768754155945995):
             if len(msg.attachments) == 0: # there are no pictures attached to this image, so it's not a selfie
                 try:
                     await msg.delete()
