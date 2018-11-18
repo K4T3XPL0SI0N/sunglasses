ef7635caf3ab61f53bfdc70f78cf22947eadaecc
boopdev <40839690+boopdev@users.noreply.github.com>
2018-11-18 19:07:03 -0400

    Update customhelp.py

--- cogs/customhelp.py
+++ cogs/customhelp.py
@@ -1,1 +1,45 @@
+import discord, asyncio
+from discord.ext import commands
 
+_commandPrefix = "j" # Replace with the bot's prefix
+
+class HelpCommands():
+    def __init__(self, client):
+        self.client = client
+        self.client.remove_command('help')
+
+    @commands.command(aliases=['h'])
+    async def help(self, ctx, *, com = None):
+
+        appInfo = await self.client.application_info()
+
+        if com == None:
+            # send regular help message, list of all commands
+            cogs = {} # this will store the cogs that exist, to help organize commands into special categories
+            for cog in self.client.cogs:
+                cogs[cog] = [] # this will store each command
+
+            for command in self.client.commands():
+                if command.cog_name in cogs:
+                    if command.hidden != True:
+                        cogs[command.cog_name].append(command) # add command to the containing cog's list
+                    elif command.hidden == True and ctx.author.id == appInfo.owner.id:
+                        cogs[command.cog_name].append(command) # if the command is hidden add it only if the help user is the bot owner
+                else:
+                    print("Uhh... Something went wrong with the help command uwu")
+
+            helpEmbed = discord.Embed(colour=0xFFFFFF, title="Help Dialog", description="`{0}` is this bot's prefix\nUse `{0}help <command>` if you'd like help on a certain command".format(_commandPrefix))
+            helpEmbed.set_thumbnail(url=appInfo.icon_url)
+            for c in cogs:
+                if len(cogs[c]) > 0:
+                    helpEmbed.add_field(name=str(c) + " [`{}`]".format(len(cogs[c])), value=", ".join(cogs[c])) # this field contains the name of the cog and all the commands in a list like, this, and, that
+                else:
+                    pass # It wouldn't add a cog that has zero commands because no help is needed for that command
+            
+            await ctx.send(embed=helpEmbed)
+        else:
+            # send help regarding the command "com" unless the command doesn't exist, then return "Command Not Found" error
+            await ctx.send("It don't work yet")
+
+def setup(client):
+    client.add_cog(HelpCommands(client))
